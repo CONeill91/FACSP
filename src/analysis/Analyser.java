b6d8641e53368ed2b304a9aaae7adcbd66bfee56
ConorONeill91 <conor.oneill63@mail.dcu.ie>
2016-04-28 23:11:12 +0100

    Adds message evaluation with respect to re-playability & deduction

--- src/analysis/Analyser.java
+++ src/analysis/Analyser.java
@@ -5,6 +5,7 @@ import model.msg.*;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Set;
 
 /**
  * The analyser creates representations of the protocol steps where an intruder E impersonates the initiator & responder.
@@ -15,8 +16,8 @@ public class Analyser {
     private Protocol protocol;
     private ArrayList<Message> initiatorImpersonation;
     private ArrayList<Message> responderImpersonation;
-    private HashSet<String> knowledge;
-    private ArrayList<Integer> path; // TODO Fill with path to data.
+    private Set<String> knowledge;
+    private ArrayList<String> path; // TODO Fill with path to data. In format A1 , B3 or C9.
 
     /**
      * Constructor.
@@ -32,8 +33,13 @@ public class Analyser {
         fillCopies();
         fillKnowledge();
         generateImpersonators();
+        System.out.println(knowledge.toString());
+        for(Message m: responderImpersonation){
+            System.out.println(evaluateMessageSend(m));
+        }
 
-
+//        extendKnowledgeOnMessageList(responderImpersonation);
+//        System.out.println(knowledge.toString());
     }
 
     /**
@@ -46,7 +52,6 @@ public class Analyser {
             initiatorImpersonation.add(copyMessage(msg));
             responderImpersonation.add(copyMessage(msg));
         }
-
     }
 
     /**
@@ -58,15 +63,16 @@ public class Analyser {
         // Adds init/resp identities
         knowledge.add(protocol.getInitiator());
         knowledge.add(protocol.getResponder());
-        // Adds what init/resp generate themselves
-        knowledge.addAll(protocol.getInitiatorGenerates());
-        knowledge.addAll(protocol.getResponderGenerates());
+        // Adds impersonator identities
+        knowledge.add("E(" + protocol.getInitiator() + ")");
+        knowledge.add("E(" + protocol.getResponder() + ")");
         // Adds public keys
         knowledge.add("PK(" + protocol.getInitiator() + ")");
         knowledge.add("PK(" + protocol.getResponder() + ")");
         knowledge.add("PK(E)");
+        knowledge.add("PK(S)");
         // Adds Intruder Private key
-        knowledge.add("SK(E)");
+        knowledge.add("ske");
 
 
     }
@@ -89,15 +95,14 @@ public class Analyser {
     public void generateInitiatorImpersonation() {
         for (Message m : initiatorImpersonation) {
             if (m.getSenderId().equals(protocol.getInitiator())) {
-                m.setSenderId("E(" + m.getSenderId() + ")");
+                m.setSenderId("E(" + protocol.getInitiator() + ")");
+            }
+            if (m.getReceiverId().equals(protocol.getInitiator())) {
+                m.setReceiverId("E(" + protocol.getInitiator() + ")");
             }
 
-            changeGeneratedValues(protocol.getInitiatorGenerates(),m);
+            //changeGeneratedValues(protocol.getInitiatorGenerates(),m);
         }
-        for (Message m : initiatorImpersonation) {
-            System.out.println(m.getSenderId() + "------->>>" + m.getReceiverId() + " : " + m.toString());
-        }
-
     }
 
     /**
@@ -108,14 +113,15 @@ public class Analyser {
     public void generateResponderImpersonation() {
         for (Message m : responderImpersonation) {
             if (m.getReceiverId().equals(protocol.getResponder())) {
-                m.setReceiverId("E(" + m.getReceiverId() + ")");
+                m.setReceiverId("E(" + protocol.getResponder() + ")");
             }
+            if (m.getSenderId().equals(protocol.getResponder())) {
+                m.setSenderId("E(" + protocol.getResponder() + ")");
+            }
+
             changeGeneratedValues(protocol.getResponderGenerates(),m);
         }
 
-        for (Message m : responderImpersonation) {
-            System.out.println(m.getSenderId() + "------->>>" + m.getReceiverId() + " : " + m.toString());
-        }
     }
 
     /**
@@ -127,6 +133,9 @@ public class Analyser {
 
     public void changeGeneratedValues(ArrayList<String> generated, Message msg) {
         for (String value : generated) {
+            if(value.startsWith("n")){
+                continue;
+            }
             if (msg instanceof Atom) {
                 if (((Atom) msg).getVarName().equals(value)) {
                     ((Atom) msg).setVarName("E(" + ((Atom) msg).getVarName() + ")");
@@ -176,7 +185,7 @@ public class Analyser {
      */
 
 
-public Message copyMessage(Message message){
+    public Message copyMessage(Message message){
         if(message instanceof Atom){
             Atom atom=new Atom(((Atom)message).getVarName());
             atom.setSenderId(message.getSenderId());
@@ -226,44 +235,191 @@ public Message copyMessage(Message message){
 }
 
     /**
-     * Evaluates whether a message may be known to an entity (Eg. for encryption does an entity possess the correct key).
+     * Extend knowledge
+     * <br>
+     * Adds the knowledge of data in the clear from each protocol step to the intruder knowledge
      * @param message Message to be evaluated
      * @see         Message
-     * @return boolean indicating whether a user may read the message successfully.
+     *
      */
 
-    public void evaluateMessage(Message message){
+    public void extendKnowledge(Message message){
         if(message instanceof Atom){
-
+           knowledge.add(((Atom) message).getVarName());
         }
 
         if(message instanceof Xor){
 
         }
 
+        if(message instanceof UnDecryptable) {
+            if(evaluateMessage(message)){
+                knowledge.add(((UnDecryptable) message).getVarName());
+            }
+        }
+
+        if(message instanceof Forward){
+            if(evaluateMessage(message)){
+                knowledge.add(((Forward) message).getVarName());
+            }
+        }
+
+        if(message instanceof Encrypt){
+           if (evaluateMessage(message)){
+               extendKnowledge(((Encrypt) message).getMessageList());
+           }
+        }
+
+        if(message instanceof MessageList){
+            for(Message msg: ((MessageList) message).getMessageList()){
+                extendKnowledge(msg);
+            }
+        }
+    }
+
+
+
+    public void extendKnowledgeOnMessageList(ArrayList<Message> messages){
+        for(int i = 0; i < messages.size(); i++) {
+            if(!evaluateMessage(messages.get(i))){
+                System.out.println("Evaluation failed at step: " + i);
+                break;
+            }
+            extendKnowledge(messages.get(i  ));
+        }
+
+
+    }
+
+
+
+    /**
+     * Evaluate whether a message may be deduced based on intruder's current knowledge.
+     * <br>
+     * @param message Message to be evaluated
+     * @see         Message
+     *
+     */
+
+    public boolean evaluateMessage(Message message){
+        if(message instanceof Atom){
+            if (knowledge.contains(inverseKey(((Atom) message).getVarName()))){
+                return true;
+            }
+        }
+
+        if(message instanceof Xor){
+
+        }
+
+        if(message instanceof UnDecryptable) {
+           return evaluateMessage(((UnDecryptable) message).getMessage());
+        }
+
+        if(message instanceof Forward){
+            return knowledge.contains(((Forward) message).getVarName());
+        }
+
+        if(message instanceof Encrypt){
+           return evaluateMessage(((Encrypt) message).getKey());
+        }
+
+        if(message instanceof MessageList){
+            boolean flag = false;
+            for(Message msg: ((MessageList) message).getMessageList()){
+                flag = evaluateMessage(msg);
+                if(!flag){
+                    break;
+                }
+            }
+            return flag;
+        }
+        return false;
+    }
+
+    public void evaluateMessageList(){
+        for(int i = 0; i < protocol.getMessages().size(); i++){
+            System.out.println("Step: " + i);
+            System.out.println(evaluateMessage(protocol.getMessages().get(i)));
+            System.out.println(evaluateMessage(initiatorImpersonation.get(i)));
+            System.out.println(evaluateMessage(responderImpersonation.get(i)));
+            System.out.println();
+        }
+    }
+
+    /**
+     * Evaluate whether a message may be sent as an impersonator, based on the attackers current knowledge. (Replayed)
+     * <br>
+     * @param message Message to be evaluated
+     * @see         Message
+     *
+     */
+
+    public boolean evaluateMessageSend(Message message){
+        if(message instanceof Atom){
+            return (knowledge.contains(((Atom) message).getVarName()));
+        }
+
+        if (message instanceof Xor){
+
+        }
+
         if(message instanceof UnDecryptable){
+            return (knowledge.contains(((UnDecryptable) message).getVarName()) && evaluateMessageSend(((UnDecryptable) message).getMessage()));
 
         }
 
         if(message instanceof Forward){
-
+            return (knowledge.contains(((Forward) message).getVarName()) && evaluateMessageSend(((Forward) message).getMessage()));
         }
 
         if(message instanceof Encrypt){
+            return (evaluateMessageSend(((Encrypt) message).getKey()) && evaluateMessageSend(((Encrypt) message).getMessageList()));
 
         }
 
         if(message instanceof MessageList){
-
+            boolean flag = false;
+            for(Message m : ((MessageList) message).getMessageList()){
+                flag = evaluateMessageSend(m);
+                if(!flag){
+                    break;
+                }
+            }
+            return flag;
         }
 
+        return false;
     }
+
+
+
+    /**
+     * Returns the inverse of the given key (Eg. PK(A) = ska & vice versa)
+     * @param key   Key to be "inversed"
+     * @return      inverse key
+     * @see         Message
+     */
+
+
+    public String inverseKey(String key){
+        if(key.startsWith("PK")){
+            return ("sk" + Character.toLowerCase(key.charAt(3))).trim();
+        }
+        if(key.startsWith("sk")){
+            return "PK(" + Character.toUpperCase(key.charAt(2)) + ")";
+        }
+        return key;
+    }
+
 
     /**
      * Returns the initiator impersonation list
      * @return      a reference to a the list
      * @see         Message
      */
+
+
 
     public ArrayList<Message> getInitiatorImpersonation() {
         return initiatorImpersonation;
--- src/view/Visualiser.java
+++ src/view/Visualiser.java
@@ -12,36 +12,37 @@ import java.util.ArrayList;
 
 /**
  * Class to model the visualiser components
- *
  */
 public class Visualiser extends JPanel {
     private ArrayList<Message> messages;
     private ArrayList<Message> initiator;
     private ArrayList<Message> responder;
     private PrettyPrinter prettyPrinter;
-    private int x = 10;
-    private int y = this.getHeight() / 2 ;
 
-    public Visualiser(ArrayList<Message> messages, ArrayList<Message> initiator, ArrayList<Message> responder ) {
+    public Visualiser(ArrayList<Message> messages, ArrayList<Message> initiator, ArrayList<Message> responder) {
         this.messages = messages;
         this.initiator = initiator;
         this.responder = responder;
         this.prettyPrinter = new PrettyPrinter();
         this.setVisible(true);
         setBackground(Color.LIGHT_GRAY);
+
     }
 
-    public void paintComponent(Graphics g){
+    public void paintComponent(Graphics g) {
+
+        int x = 50;
+        int y = 75;
         super.paintComponent(g);
         g.setFont(new Font("Serif", Font.BOLD, 16));
-        for(int i = 0; i < messages.size(); i++){
-            g.drawString(i + ". " + messages.get(i).getSenderId() + "-->" + messages.get(i).getReceiverId() + ": " +  prettyPrinter.createPrettyMessage(messages.get(i)),x,y);
-            g.drawString(i + ". " + initiator.get(i).getSenderId() + "-->" + initiator.get(i).getReceiverId() + ": " + prettyPrinter.createPrettyMessage(initiator.get(i)),300,y);
-            g.drawString(i + ". " + responder.get(i).getSenderId() + "-->" + responder.get(i).getReceiverId() + ": " + prettyPrinter.createPrettyMessage(responder.get(i)),600,y);
-            y += 20;
+
+        g.drawString("Original Message List" + "                  " + "Initiator Impersonation" + "\t\t" + "Responder Impersonation", x - 20, y - 20);
+        for (int i = 0; i < messages.size(); i++) {
+            g.drawString(i + ". " + messages.get(i).getSenderId() + "-->" + messages.get(i).getReceiverId() + ": " + prettyPrinter.createPrettyMessage(messages.get(i)), x, y);
+            g.drawString(i + ". " + initiator.get(i).getSenderId() + "-->" + initiator.get(i).getReceiverId() + ": " + prettyPrinter.createPrettyMessage(initiator.get(i)), 300, y);
+            g.drawString(i + ". " + responder.get(i).getSenderId() + "-->" + responder.get(i).getReceiverId() + ": " + prettyPrinter.createPrettyMessage(responder.get(i)), 600, y);
+            y += 30;
         }
+
     }
-
-
-
 }
